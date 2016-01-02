e343c0bf17dca8c87f1ce385ae7488d9cd4d2af3
Webs <weberswords@gmail.com>
2016-01-01 20:55:56 -0800

    Updated tuition and tuition_input

--- tuition.py
+++ tuition.py
@@ -7,57 +7,56 @@ house_fee = 3500
 meal_fee = 2000
 rCredit = 130
 nrCredit = 285
+tuition = 0
+stud_done = 0
 final_total = 0
+invalid_request = 0
+# stud_processed = tuition_input.readline()
+# stud_processed = int(stud_processed)
+stud_processed = 3
+print("Students to process: %d" % stud_processed)
 
 for line in tuition_input:
+    total_due = 0
     line = line.split()
-    if len(line) == 1:
-        stud_processed = int(line[0])
-        print("Students to process: %d" % stud_processed)
-    elif len(line) > 1:
-        stud_done = 0
-        total_due = 0
-        tuition = 0
-        invalid_request = 0
-        # while stud_done < stud_processed:
+    if len(line) > 1:
         student_ID = line[0]
-        print("\nStudent ID: %s" % student_ID)
         initials = line[1]
-        print("Student Initials: %s" % initials.upper())
+        credits = int(line[2])
         residency = line[3]
+        print("\nStudent ID: %s" % student_ID)
+        print("Student Initials: %s" % initials.upper())
         if residency.upper() == "R":
             tuition = int(line[2])*rCredit
+            total_due += tuition
             print("Residency: resident")
         elif residency.upper() == "N":
-            tuition = int(line[2])*nrCredit
+            tuition = credits*nrCredit
+            total_due += tuition
             total_due += nrfee
-            housing = line[4]
             print("Residency: non-resident")
+            housing = line[4]
             if housing.upper() == "O":
-                continue
+                pass
             elif housing.upper() == "H":
                 total_due += house_fee
-                meal_plan = list[5]
                 print("Housing: $ %d" % house_fee)
+                meal_plan = line[5]
                 if meal_plan.upper() == "N":
-                    continue
+                    pass
                 elif meal_plan.upper() == "M":
                     total_due =+ meal_fee
                     print("Meals: $ %d" % meal_fee)
-        # if int(list[2]) < 1:
-        #     print("Insufficient credits.")
-        #     invalid_request += 1
-        # elif int(list[2]) > 21:
-        #     print("Too many credits.")
-        #     invalid_request += 1
-        else:
-            print("Credits: %d" % list[2])
+            else:
+                pass
+        print("Credits: %d" % credits)
         print("Tuition: $ %d" % tuition)
         print("Total Due: $ %d" % total_due)
-        stud_done += 1
-        final_total += total_due
+    final_total += total_due
+    stud_done += 1
+    print(stud_done)
 
-print("FINAL TOTALS")
+print("\nFINAL TOTALS")
 print("Valid request count: %d" % stud_processed)
 print("Invalid request count: %d" % invalid_request)
 print("Total fees due: $ %d" % final_total)
